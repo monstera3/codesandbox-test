36f1eb8e1801d4435b71b628de57b727059763b9
suna <96680507+monstera3@users.noreply.github.com>
2022-03-27 13:41:25 +0900

    let

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 // const let 変数宣言
-var val1 = "var変数";
-console.log(val1);
+// var val1 = "var変数";
+// console.log(val1);
+let val2 = "let変数";
+console.log(val2);
+
+// let上書き
+val2 = "let上書き";
+console.log(val2);
+
+// 再宣言は不可能
