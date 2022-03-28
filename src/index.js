d3872e06358fdda6245f751e81552f2e85b66429
suna <96680507+monstera3@users.noreply.github.com>
2022-03-28 12:54:43 +0900

    分割代入プロパティ

--- src/index.js
+++ src/index.js
@@ -41,3 +41,7 @@ const myProfile = {
 
 const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
 console.log(message1);
+
+const { name, age } = myProfile;
+const message2 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message2);
